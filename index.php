32d0ab87981abc6fab572e1a3fa3e9fb691f8873
meetk <1438311@stu.ucenmanchester.ac.uk>
2024-09-20 11:17:52 +0100

    html

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 $hello = 'Hello World';
 echo "test $hello";
 ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=S, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
